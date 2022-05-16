13ddf44b7f7388c73ea65eca11ad89f8bf4f174b
Arif.Bozdag <arif.bozdag@logo.com.tr>
2022-05-16 17:28:41 +0300

    Fix: generate_external_share

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,13 +1,21 @@
 import os
+from unittest import TestCase
 from dys_connector.dys_api_manager import DYSManager
 
 DYS_ENDPOINT = os.environ['DYS_ENDPOINT']
 DYS_TOKEN = os.environ['DYS_TOKEN']
 DYS_CLEAR_DIR = os.environ['DYS_CLEAR_DIR']
+DOC_CID = os.environ['DOC_CID']
 
 
-def test_clear_directory():
-    manager = DYSManager(DYS_ENDPOINT, DYS_TOKEN)
-    manager.clear_directory(DYS_CLEAR_DIR)
-    dir_list = manager.get_dir_structure(DYS_CLEAR_DIR)
-    assert dir_list == []
+class TestIntegration(TestCase):
+    def test_generate_external_share(self):
+        manager = DYSManager(DYS_ENDPOINT, DYS_TOKEN)
+        external_url = manager.generate_external_share(DOC_CID, idm_ex_share=False)
+        self.assertIn(DOC_CID, external_url)
+
+    def test_clear_directory(self):
+        manager = DYSManager(DYS_ENDPOINT, DYS_TOKEN)
+        manager.clear_directory(DYS_CLEAR_DIR)
+        dir_list = manager.get_dir_structure(DYS_CLEAR_DIR)
+        self.assertEqual(dir_list, [])
